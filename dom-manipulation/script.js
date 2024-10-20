fd5d357151b8fc9bfed76368dc502ca9b6060036
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-10-20 19:21:48 +0000

    final commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,27 +1,43 @@
 // Create quotes Array:
 
 let quotes = [
-    { text: "Life is like a box of chocolates. You never know what you're gonna get.", category: "Life" },
-    { text: "Believe you can and you're halfway there.", category: "Self-belief" },
-    { text: "The greatest accomplishment is not in never failing, but in rising up every time we fall.", category: "Success" },
-    { text: "You miss 100% of the shots you don't take.", category: "Motivation" }
-]
+  {
+    text: "Life is like a box of chocolates. You never know what you're gonna get.",
+    category: "Life",
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    category: "Self-belief",
+  },
+  {
+    text: "The greatest accomplishment is not in never failing, but in rising up every time we fall.",
+    category: "Success",
+  },
+  {
+    text: "You miss 100% of the shots you don't take.",
+    category: "Motivation",
+  },
+];
 
 function showRandomQuote() {
-    let number = quotes.length;
-    let randomIndex = Math.floor(Math.random() * number);
+  let number = quotes.length;
+  let randomIndex = Math.floor(Math.random() * number);
 
-    let quotesDisplay = document.getElementById('quoteDisplay')
-    let textContent = quotes[randomIndex] 
-    quotesDisplay.innerHTML = textContent.text + '<br/>' + textContent.category
-
+  let quotesDisplay = document.getElementById("quoteDisplay");
+  let textContent = quotes[randomIndex];
+  quotesDisplay.innerHTML = textContent.text + "<br/>" + textContent.category;
 }
-let newQuoteBtn = document.getElementById('newQuote')
-newQuoteBtn.addEventListener('click',showRandomQuote);
+let newQuoteBtn = document.getElementById("newQuote");
+newQuoteBtn.addEventListener("click", showRandomQuote);
 console.log(showRandomQuote());
 
-function createAddQuoteForm(){
-    let newQuoteText = document.getElementById('newQuoteText').value
-    let newQuoteCategory = document.getElementById('newQuoteCategory').value
-   quotes.push({text : newQuoteText , category : newQuoteCategory})
+function createAddQuoteForm() {
+  let newQuoteText = document.getElementById("newQuoteText").value;
+  let newQuoteCategory = document.getElementById("newQuoteCategory").value;
+  quotes.push({ text: newQuoteText, category: newQuoteCategory });
+
+  let div = document.createElement("div");
+  div.appendChild = textContent.text + "<br/>" + textContent.category;
+  let quotesDisplay = document.getElementById("quoteDisplay");
+  quotesDisplay.innerHTML = div;
 }
